28d35672d9450575a9a0ba01c531dbe7454b3013
Allyson Henrique Alves <54608221+allyson-alves@users.noreply.github.com>
2022-06-15 18:59:51 -0300

    main

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,4 +1,4 @@
-it.only('Testa a página de política de privacidade de forma independente', function(){
+it('Testa a página de política de privacidade de forma independente', function(){
     cy.visit('./src/privacy.html')
 
     cy.contains('Talking About Testing').should('be.visible')
